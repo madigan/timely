3c1aedf8b4ad3974a8da6ae5d4c4842b75167a03
John Lynn <lynnjonathanp@gmail.com>
2025-09-03 10:00:07 -0400

    fix: update copyright year

--- packages/frontend/src/components/Footer.vue
+++ packages/frontend/src/components/Footer.vue
@@ -1,7 +1,11 @@
+<script setup>
+const year = new Date().getFullYear();
+</script>
+
 <template>
   <footer class="footer footer-center p-8 bg-base-300 text-base-content">
     <div>
-      <p>&copy; 2024 Timely. Built with Vue 3, Tailwind CSS, and DaisyUI.</p>
+      <p>&copy; {{year}} Timely. Built with Vue 3, Tailwind CSS, and DaisyUI.</p>
     </div>
   </footer>
 </template>
